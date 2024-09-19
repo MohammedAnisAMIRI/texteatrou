a43b0a3a9b19967460e3bb299b708bd124c34838
MohammedAnisAMIRI <les.amiri59@gmail.com>
2024-09-19 14:11:52 +0200

    modif

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -7,12 +7,13 @@ public class App {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
         System.out.println("entrez le premiers nom commun");
+
         String NOM1 = clavier.next();
         System.out.println("entrer le nombre : ");
         int NOMBRE = clavier.nextInt();
         System.out.println("entrez le deuxieme nom commun");
         String NOM2 = "test2";
-        String ADJECTIF = "teste2";
+        String ADJECTIF = "teste4";
         System.out.println(
                 "il etait une fois" + NOM1 + " " + "qui avait" + " " + NOMBRE + " " + NOM2 + " " + "cela la reduit trés"
                         + ADJECTIF + ".");
